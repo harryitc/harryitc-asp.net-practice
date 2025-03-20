0c401ac7c7c9765f1d998037363d30d5d7636550
Phan Ngọc Cường <cuongharryit@gmail.com>
2025-03-21 03:10:45 +0700

    Update product view and constants to enforce admin-only access for certain actions

--- Views/Shared/Constants.cs
+++ Views/Shared/Constants.cs
@@ -5,8 +5,8 @@
     new MenuItem { Title = "Trang chủ", Controller = "Home", Action = "Index" },
     new MenuItem { Title = "Sản phẩm", Controller = "Product", Action = "Index"},
     // new MenuItem { Title = "Sản phẩm", Controller = "Product", Action = "Index", Roles = new[] { RoleUser.Customer, RoleUser.Admin } },
-    new MenuItem { Title = "Danh mục", Controller = "Category", Action = "Index" }, // Chỉ Admin
-    new MenuItem { Title = "Hình ảnh", Controller = "ProductImage", Action = "Index" } // Chỉ Admin
+    new MenuItem { Title = "Danh mục", Controller = "Category", Action = "Index", Roles = new[] { RoleUser.Admin } }, // Chỉ Admin
+    new MenuItem { Title = "Hình ảnh", Controller = "ProductImage", Action = "Index", Roles = new[] { RoleUser.Admin } } // Chỉ Admin
 };
 
     public static readonly string AUTHOR_NAME = "2280600358_PhanNgocCuong";
