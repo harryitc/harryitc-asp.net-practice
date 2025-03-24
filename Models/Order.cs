832f82d19ccd3922fa61d615f3332948d94c9725
Phan Ngọc Cường <cuongharryit@gmail.com>
2025-03-24 16:02:48 +0700

    Remove commented-out User property from Order model

--- Models/Order.cs
+++ Models/Order.cs
@@ -2,7 +2,7 @@
     {
         public int Id { get; set; }
         public string UserId { get; set; }
-        public ApplicationUser User { get; set; }
+        // public ApplicationUser User { get; set; }
 
         public DateTime OrderDate { get; set; } = DateTime.Now;
         public decimal TotalPrice { get; set; }
