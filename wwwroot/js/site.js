0cf073fd64d05a5e540da46db8df495db3ccfc8c
Phan Ngọc Cường <cuongharryit@gmail.com>
2025-03-28 10:29:53 +0700

    Add timeline styles and success notification for order completion

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function showOrderSuccess(orderId) {
+    Swal.fire({
+        title: 'Đặt hàng thành công!',
+        text: `Mã đơn hàng của bạn là #${orderId}`,
+        icon: 'success',
+        showConfirmButton: true,
+        confirmButtonText: 'Xem chi tiết đơn hàng',
+        showCancelButton: true,
+        cancelButtonText: 'Tiếp tục mua sắm'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = `/Order/Details/${orderId}`;
+        } else {
+            window.location.href = '/Product';
+        }
+    });
+}
